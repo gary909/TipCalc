8de46edb080c79ae23b25676fcd5349bea59121e
gary <gary.white@lodgit.com>
2021-08-17 17:32:02 +0200

    styling

--- code.js
+++ code.js
@@ -3,8 +3,6 @@ function getDinnerValue() {
     var dinPerc = document.getElementById("TipPerc").value;
     var peoPres = document.getElementById("PeoplePres").value;
     var tip = (dinTot / 100) * dinPerc / peoPres;
-    var result = "A " + dinPerc + "% tip of $" + dinTot + " divided by " + peoPres + " people" + " equals a $" + tip.toFixed(2) + " tip per person!";;
+    var result =  "A " + dinPerc + "% tip of $" + dinTot + " divided by " + peoPres + " people" + " equals a $" + tip.toFixed(2) + " tip per person! Have a nice day";
     document.getElementById("output").innerHTML = result;
   }
-
-// console.log(tipCalc(100, 10, 2)); // bill amount, tip percentage, amount of people
